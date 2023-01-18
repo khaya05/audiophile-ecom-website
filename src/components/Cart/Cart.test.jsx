4d53316b0ea1b99e1c4e5fec9ac29e7c0b97408c
khaya05 <khayamnyandu5@gmail.com>
2023-01-18 21:41:38 +0200

    added more cart tests

--- src/components/Cart/Cart.test.jsx
+++ src/components/Cart/Cart.test.jsx
@@ -2,12 +2,18 @@ import { render, screen } from '../../test-utils';
 import Cart from './Cart';
 
 describe('Cart', () => {
-  it('renders correctly', () => {
+  it('heading renders correctly when cart is empty', () => {
     render(<Cart />);
     const headingElement = screen.getByRole('heading', {
       level: 3,
       name: 'cart (0)',
     });
     expect(headingElement).toBeInTheDocument();
+  });
+
+  it('paragraph renders correctly when cart is empty', () => {
+    render(<Cart />);
+    const paragraphElement = screen.getByText('your cart is currently empty');
+    expect(paragraphElement).toBeInTheDocument();
   });
 });
