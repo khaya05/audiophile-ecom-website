27f9427ed90c1398f7e32b81f72facb52351bd65
khaya05 <khayamnyandu5@gmail.com>
2023-01-20 15:29:59 +0200

    removed comments

--- src/components/shared/Audiophile.test.jsx
+++ src/components/shared/Audiophile.test.jsx
@@ -14,7 +14,7 @@ describe('Audiophile', () => {
     expect(headingElement).toBeInTheDocument();
   });
 
-  it.skip('paragraph to render correctly', () => {
+  it('paragraph renders correctly', () => {
     render(<Audiophile />);
     const paragraphElement =
       screen.getByText(`Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.`);
